9be06b36c5b147c0193a2791beedae49009613fc
Andrew Cucuzza <Andrew@wifi-roaming-128-4-148-157.host.udel.edu>
2018-10-25 10:07:08 -0400

    style update

--- src/app/modules/views/forecast/forecast.component.ts
+++ src/app/modules/views/forecast/forecast.component.ts
@@ -22,21 +22,18 @@ export class ForecastComponent {
 
   searchCity() {
     this._http.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.zipcode + "&APPID=de84f1f1eaffc87826f0a6d42094445f")
-
   .subscribe(
-      (res: Response) => {
-        const weatherCity = res.json();
-        console.log(weatherCity);
-        this.cityName = weatherCity.name;
-        this.description = weatherCity.weather[0].description;
-        this.temp = (weatherCity.main.temp * (9/5) - 459.67).toFixed(1);
-        this.windspeed = (weatherCity.wind.speed * 2.237).toFixed(1);
-        this.high = (weatherCity.main.temp_max * (9/5) - 459.67).toFixed(1);
-        this.low = (weatherCity.main.temp_min * (9/5) - 459.67).toFixed(1);
+    (res: Response) => {
+      const weatherCity = res.json();
+      console.log(weatherCity);
+      this.cityName = weatherCity.name;
+      this.description = weatherCity.weather[0].description;
+      this.temp = (weatherCity.main.temp * (9/5) - 459.67).toFixed(1);
+      this.windspeed = (weatherCity.wind.speed * 2.237).toFixed(1);
+      this.high = (weatherCity.main.temp_max * (9/5) - 459.67).toFixed(1);
+      this.low = (weatherCity.main.temp_min * (9/5) - 459.67).toFixed(1);
       }
     )
-
   }
-
   title = 'Weather App';
 }
--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() { 
+  }
+  city = ""
   ngOnInit() {
   }
-
 }
